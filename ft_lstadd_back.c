ae688e864efee0b320b82ff290b0496acb25ac47
Eugene Vlasov <vlasov3454@yandex.ru>
2021-02-28 16:36:34 +0300

    lstadd_back modified

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -14,16 +14,18 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list *last;
+	t_list		*start;
 
-	if (lst)
+	if (lst && (*lst) && new)
 	{
-		if (*lst)
+		start = (*lst);
+		if (start != NULL)
 		{
-			last = ft_lstlast(*lst);
-			last->next = new;
+			while (start->next)
+				start = start->next;
+			start->next = new;
 		}
 		else
-			*lst = new;
+			(*lst) = new;
 	}
 }
